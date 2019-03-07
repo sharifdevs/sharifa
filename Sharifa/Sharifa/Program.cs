a2a9af7214a9bc19b1440ed67365ac2ef0c43ebf
rezanmz <rezanmz@ymail.com>
2019-03-07 11:21:45 +0330

    in goho

--- Sharifa/Sharifa/Program.cs
+++ Sharifa/Sharifa/Program.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Telegram.Bot;
 
 namespace Sharifa
 {
     static class Program
     {
+        public static TelegramBotClient thisBot = new TelegramBotClient("775773227");
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
