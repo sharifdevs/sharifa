487f0d2d6f316bdf3df6389b411fdf2712d24768
rezanmz <rezanmz@ymail.com>
2019-03-07 14:18:37 +0330

    1

--- Sharifa/Sharifa/Program.cs
+++ Sharifa/Sharifa/Program.cs
@@ -16,6 +16,7 @@ namespace Sharifa
         [STAThread]
         static void Main()
         {
+            // test
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
